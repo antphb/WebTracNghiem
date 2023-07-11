2887381ec97f7844eb08ce29836a68958491b20f
antphb <thanhnd2462245@gmail.com>
2023-07-11 13:36:21 +0700

    second

--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call D:\Seme\Year_3_Semester_3\WebTracNghiem\Web\.venv\Scripts\activate.bat
-
-python .\manage.py makemigrations 
-python .\manage.py migrate
-python .\manage.py runserver
